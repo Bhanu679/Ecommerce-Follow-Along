2517db1f3544e7c66fdba07740763b4510aa62de
siva kumar kalagandha <bhanusree414@gmail.com>
2025-02-13 09:56:10 +0530

    added files

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,5 @@
 export default {
-    plugins: {
-      "@tailwindcss/postcss":{},
-    },
-<<<<<<< HEAD
-  };
-=======
-  };
-  
->>>>>>> 5ab5988290a63c60ed3f54790d5a57e91f80d250
+  plugins: {
+    "@tailwindcss/postcss":{},
+  },
+};
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,15 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './Pages/Login';
-
-const AppRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default AppRoutes;
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
+export {LoginPage,SignupPage};
