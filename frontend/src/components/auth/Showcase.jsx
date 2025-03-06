f82c20f05da7ccd1dbf6da30fa98ec607ff1b7fc
siva kumar kalagandha <bhanusree414@gmail.com>
2025-03-06 09:33:19 +0530

    edit function added

---
+++ frontend/src/components/auth/Showcase.jsx
@@ -0,0 +1,13 @@
+function Showcase() {
+  return (
+    <div className="bg-gray-700 flex justify-center items-center py-6">
+      <img
+        src="https://relay-bl-in-records.sgp1.cdn.digitaloceanspaces.com/GameNation/AdAsset_101"
+        alt="Showcase"
+        className="w-[95%] max-w-4xl rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
+      />
+    </div>
+  );
+}
+
+export default Showcase;
