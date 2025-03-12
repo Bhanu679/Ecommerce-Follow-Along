a4b099641702e5e2f4bcd34049254cfb79937833
siva kumar kalagandha <bhanusree414@gmail.com>
2025-03-12 11:01:37 +0530

    added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,15 @@
 import Product from "./auth/Product";
-// import Showcase from "./auth/Showcase";
 import Navbar from "./Navbar";
 const Home = () => {
   // Example list of products
   return(
-    <div className="flex flex-col gap-10 bg-purple-700">
+    <div className="flex flex-col gap-2 bg-yellow-400">
       <div>
     <Navbar/>
     </div>
-    {/* <div className="mt-15">
-    <Showcase/>
-    </div> */}
+    <div className="mt-15">
+    
+    </div>
     <Product />
     
     </div>
